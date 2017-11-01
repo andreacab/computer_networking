c86e7d6aa17fded7d0946be3f7cf89788c6bb2c5
Andrea Cabral <andrea.cabralb@gmail.com>
2017-11-01 05:49:39 -0700

    implement web server assignment 1 of ch2

--- ch2/assignments/web_server.py
+++ ch2/assignments/web_server.py
@@ -4,20 +4,24 @@ import sys # In order to terminate the program
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
-#Fill in start
-#Fill in end
+HOST = '' # INADDR_ANY
+PORT = 6789
+serverSocket.bind((HOST, PORT))
+serverSocket.listen(1)
 while True:
     #Establish the connection
     print('Ready to serve...')
-    connectionSocket, addr =   #Fill in start              #Fill in end
+    connectionSocket, addr = serverSocket.accept()
+
     try:
-        message =   #Fill in start          #Fill in end
+        message = connectionSocket.recv(2048)
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start       #Fill in end
+        outputdata = f.read()
+
         #Send one HTTP header line into socket
-        #Fill in start
-        #Fill in end
+        connectionSocket.send("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n".encode())
+
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
@@ -26,10 +30,10 @@ while True:
         connectionSocket.close()
     except IOError:
         #Send response message for file not found
-        #Fill in start
-        #Fill in end
+        connectionSocket.send("HTTP/1.1 404 NOT FOUND\r\n\r\n")
+
         #Close client socket
-        #Fill in start
-        #Fill in end
+        connectionSocket.close()
+
 serverSocket.close()
 sys.exit()#Terminate the program after sending the corresponding data
